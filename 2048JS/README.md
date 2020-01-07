f8fd2702edfeb8fd7910a1bde85f6821c81382be
Mayan <mshoshan@uci.edu>
2020-01-07 00:20:44 -0800

    2048

---
+++ 2048JS/README.md
@@ -0,0 +1,2 @@
+# 2048_JS
+the game 2048 made in js, with scalable grid
---
+++ 2048JS/script.js
@@ -0,0 +1,261 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var sizeInput = document.getElementById('size');
+var changeSize = document.getElementById('change-size');
+var scoreLabel = document.getElementById('score');
+
+var score = 0;
+var size = 4;
+var width = canvas.width / size - 6;
+var cells = [];
+var fontSize;
+var loss = false;
+var win =false;
+startGame();
+
+changeSize.onclick = function () {
+  if (sizeInput.value >= 2 && sizeInput.value <= 20) {
+    size = sizeInput.value;
+    width = canvas.width / size - 6;
+    console.log(sizeInput.value);
+    canvasClean();
+    startGame();
+  }
+}
+
+function cell(row, coll) {
+  this.value = 0;
+  this.x = coll * width + 5 * (coll + 1);
+  this.y = row * width + 5 * (row + 1);
+}
+
+function createCells() {
+  var i, j;
+  for(i = 0; i < size; i++) {
+    cells[i] = [];
+    for(j = 0; j < size; j++) {
+      cells[i][j] = new cell(i, j);
+    }
+  }
+}
+
+function drawCell(cell) {
+  ctx.beginPath();
+  ctx.rect(cell.x, cell.y, width, width);
+  switch (cell.value){
+    case 0 : ctx.fillStyle = '#A9A9A9'; break;
+    case 2 : ctx.fillStyle = '#D2691E'; break;
+    case 4 : ctx.fillStyle = '#FF7F50'; break;
+    case 8 : ctx.fillStyle = '#ffbf00'; break;
+    case 16 : ctx.fillStyle = '#bfff00'; break;
+    case 32 : ctx.fillStyle = '#40ff00'; break;
+    case 64 : ctx.fillStyle = '#00bfff'; break;
+    case 128 : ctx.fillStyle = '#FF7F50'; break;
+    case 256 : ctx.fillStyle = '#0040ff'; break;
+    case 512 : ctx.fillStyle = '#ff0080'; break;
+    case 1024 : ctx.fillStyle = '#D2691E'; break;
+    case 2048 : ctx.fillStyle = '#FF7F50'; break;
+    default : ctx.fillStyle = '#ff0080';
+  }
+  ctx.fill();
+  if (cell.value) {
+    fontSize = width / 2;
+    ctx.font = fontSize + 'px Arial';
+    ctx.fillStyle = 'white';
+    ctx.textAlign = 'center';
+    ctx.fillText(cell.value, cell.x + width / 2, cell.y + width / 2 + width/7);
+  }
+}
+
+function canvasClean() {
+  ctx.clearRect(0, 0, 500, 500);
+}
+
+
+document.onkeydown = function (event) {
+  if (!loss && !win) {
+    if (event.keyCode === 38 || event.keyCode === 87) {
+      moveUp(); 
+    } else if (event.keyCode === 39 || event.keyCode === 68) {
+      moveRight();
+    } else if (event.keyCode === 40 || event.keyCode === 83) {
+      moveDown(); 
+    } else if (event.keyCode === 37 || event.keyCode === 65) {
+      moveLeft(); 
+    }
+    if(!loss){
+    scoreLabel.innerHTML = 'Score : ' + score;}
+    else{
+        scoreLabel.innerHTML ='YOU LOSE, NO MORE MOVES';
+    }
+    if(win){ scoreLabel.innerHTML ='YOU WIN';}
+  }
+  
+}
+
+function startGame() {
+  scoreLabel.innerHTML = 'Get to Tile 2048 to Win!'
+  createCells();
+  drawAllCells();
+  pasteNewCell();
+}
+
+function loseGame() {
+  canvas.style.opacity = '0.5';
+  loss = true;
+}
+
+function winCheck(){
+  for(var i=0;i<cells.length();i++){
+    for(var j=0;j<cells.length();j++){
+    if(cells[i][j].value===2048){
+        scoreLabel.innerHTML ='YOU WIN';
+  canvas.style.opacity = '0.5';
+  win = true;
+  return;
+}
+    }
+  }
+}
+
+
+function drawAllCells() {
+  var i, j;
+  for(i = 0; i < size; i++) {
+    for(j = 0; j < size; j++) {
+      drawCell(cells[i][j]);
+    }
+  }
+}
+
+function pasteNewCell() {
+  
+  var countFree = 0;
+  var i, j;
+  for(i = 0; i < size; i++) {
+    for(j = 0; j < size; j++) {
+      if(!cells[i][j].value) {
+        countFree++;
+      }
+    }
+  }
+  if(countFree===0) {
+    loseGame();
+    return;
+  }
+  while(true) {
+    var row = Math.floor(Math.random() * size);
+    var coll = Math.floor(Math.random() * size);
+    if(!cells[row][coll].value) {
+      cells[row][coll].value = 2 * Math.ceil(Math.random() * 2);
+      drawAllCells();
+      return;
+    }
+  }
+}
+
+function moveRight () {
+  var i, j;
+  var coll;
+  for(i = 0; i < size; i++) {
+    for(j = size - 2; j >= 0; j--) {
+      if(cells[i][j].value) {
+        coll = j;
+        while (coll + 1 < size) {
+          if (!cells[i][coll + 1].value) {
+            cells[i][coll + 1].value = cells[i][coll].value;
+            cells[i][coll].value = 0;
+            coll++;
+          } else if (cells[i][coll].value == cells[i][coll + 1].value) {
+            cells[i][coll + 1].value *= 2;
+            score +=  cells[i][coll + 1].value;
+            cells[i][coll].value = 0;
+            break;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+  }
+  pasteNewCell();
+}
+
+function moveLeft() {
+  var i, j;
+  var coll;
+  for(i = 0; i < size; i++) {
+    for(j = 1; j < size; j++) {
+      if(cells[i][j].value) {
+        coll = j;
+        while (coll - 1 >= 0) {
+          if (!cells[i][coll - 1].value) {
+            cells[i][coll - 1].value = cells[i][coll].value;
+            cells[i][coll].value = 0;
+            coll--;
+          } else if (cells[i][coll].value == cells[i][coll - 1].value) {
+            cells[i][coll - 1].value *= 2;
+            score +=   cells[i][coll - 1].value;
+            cells[i][coll].value = 0;
+            break;
+          } else {
+            break; 
+          }
+        }
+      }
+    }
+  }
+  pasteNewCell();
+}
+
+function moveUp() {
+  var i, j, row;
+  for(j = 0; j < size; j++) {
+    for(i = 1; i < size; i++) {
+      if(cells[i][j].value) {
+        row = i;
+        while (row > 0) {
+          if(!cells[row - 1][j].value) {
+            cells[row - 1][j].value = cells[row][j].value;
+            cells[row][j].value = 0;
+            row--;
+          } else if (cells[row][j].value == cells[row - 1][j].value) {
+            cells[row - 1][j].value *= 2;
+            score +=  cells[row - 1][j].value;
+            cells[row][j].value = 0;
+            break;
+          } else {
+            break; 
+          }
+        }
+      }
+    }
+  }
+  pasteNewCell();
+}
+
+function moveDown() {
+  var i, j, row;
+  for(j = 0; j < size; j++) {
+    for(i = size - 2; i >= 0; i--) {
+      if(cells[i][j].value) {
+        row = i;
+        while (row + 1 < size) {
+          if (!cells[row + 1][j].value) {
+            cells[row + 1][j].value = cells[row][j].value;
+            cells[row][j].value = 0;
+            row++;
+          } else if (cells[row][j].value == cells[row + 1][j].value) {
+            cells[row + 1][j].value *= 2;
+            score +=  cells[row + 1][j].value;
+            cells[row][j].value = 0;
+            break;
+          } else {
+            break; 
+          }
+        }
+      }
+    }
+  }
+  pasteNewCell();
+}
